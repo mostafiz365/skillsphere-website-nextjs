57124a685f159e2e12b7ff69f082049c1c48df7e
mostafiz365 <rahmanmostafiz365@gmail.com>
2026-04-30 12:11:29 +0600

    create navbar and add some json data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
@@ -20,9 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${outfit.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
+        {children}
+        </body>
     </html>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+
+    const links = <>
+    <li><Link href="/">Home</Link></li>
+    <li><Link href="/courses">Courses</Link></li>
+    <li><Link href="/profile">My Profile</Link></li>
+
+    </>
+    return (
+        <div>
+            <div className="navbar bg-[#292654] shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {links}
+      </ul>
+    </div>
+    <h2 className='text-xl font-bold text-white'>SkillSphere</h2>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 text-white font-semibold">
+      {links}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <Link href="/signup">
+        <button className="bg-[#4737b0] text-white rounded-full py-2 px-4">SignUp</button>
+    </Link>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
